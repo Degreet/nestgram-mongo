89ef7458b4fcb0b26854ebe3e9fe0a63314fd9af
Degreet <degreetpro@gmail.com>
2022-06-04 11:21:12 +0200

    export module

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ import 'reflect-metadata';
 export * from './types/schema.types';
 export * from './decorators/prop.decorator';
 export * from './decorators/schema.decorator';
+
+export * from './modules/connection.module';
+export * from './modules/mongo.module';
